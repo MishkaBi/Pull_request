c440c9f0c86f213eeecc12d22ec3a431cc1ab3ee
Misha Biryuk <vovann7093@gmail.com>
2021-07-11 15:22:27 +0300

    second commit

---
+++ Monobank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Monobank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("For how many months?");
+            int month = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= month; i++)
+            {
+                amount += amount / 100 * 14;                
+            }
+            Console.WriteLine($"After {month} months your amount will be {amount}.");
+        }
+    }
+}
